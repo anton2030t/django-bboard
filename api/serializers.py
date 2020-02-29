23830a176d599b491b724202dacf7194e013ca2b
Anton Larchenko <anton2030t@gmail.com>
2020-03-01 00:11:40 +0300

    Add files via upload

---
+++ api/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from easy_thumbnails.files import get_thumbnailer
+
+from main.models import Bb, Comment
+
+class BbSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bb
+        fields = ('id', 'title', 'content', 'price', 'created_at')
+
+class BbDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bb
+        fields = ('id', 'title', 'content', 'price', 'created_at', 'contacts', 'image')
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('bb', 'author', 'content', 'created_at')
